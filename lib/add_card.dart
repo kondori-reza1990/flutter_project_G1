612324c99aee2a8dfe717ea8e7ca498dadaf3e65
Alipour <kondori.reza1990@gmail.com>
2022-12-03 19:13:17 +0330

    project update 2022-12-3

--- lib/add_card.dart
+++ lib/add_card.dart
@@ -8,38 +8,106 @@ class AddCard extends StatefulWidget {
 }
 
 class _AddCardState extends State<AddCard> {
+  bool changeState = true;
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      child: Stack(
-        children: [
-          Image.asset('assets/images/image1.png'),
-          Align(
-            alignment: Alignment.bottomLeft,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(50)
-                    )
-                ),
-                height: MediaQuery.of(context).size.height * 3 /4,
-              )
-    ),
-          Positioned(
-            child: FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.white,
-              child: Icon(
-                  Icons.favorite_border,
-                color: Colors.indigo,
-              ),
+    return Scaffold(
+      body: Container(
+        color: Colors.indigo,
+        child: Stack(
+          children: [
+            Image.asset('assets/images/image1.png'),
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(50)
+                      )
+                  ),
+                  padding: const EdgeInsets.only(
+                    top: 40,
+                    left: 20,
+                    right: 20,
+                    bottom: 20,
+                  ),
+                  height: MediaQuery.of(context).size.height * 3 /4,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: const InputDecoration(
+                              labelText: "Card Number",
+                            prefixIcon: Icon(Icons.credit_card)
+                          ),
+                          controller: controller,
+                          //expands: true,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: TextField(
+                                decoration: const InputDecoration(
+                                    labelText: "Expiry Date",
+                                    prefixIcon: Icon(Icons.credit_card)
+                                ),
+                                controller: controller,
+                                //expands: true,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: TextField(
+                                decoration: const InputDecoration(
+                                    labelText: "CVV2",
+                                    prefixIcon: Icon(Icons.credit_card)
+                                ),
+                                controller: controller,
+                                //expands: true,
+                              ),
+                            ),
+                          ],
+                        ),
+                        TextField(
+                          decoration: const InputDecoration(
+                              labelText: "Name",
+                              prefixIcon: Icon(Icons.credit_card)
+                          ),
+                          controller: controller,
+                          //expands: true,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+
+                            },
+                            child: Text("Save"))
+                      ],
+                    ),
+                  ),
+                )
             ),
-            top: (MediaQuery.of(context).size.height * 1 /4) - 32,
-            right: MediaQuery.of(context).size.width * 1 / 7,
-          )
-        ],
+            Positioned(
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    changeState = !changeState;
+                  });
+                },
+                backgroundColor: Colors.white,
+                child: Icon(
+                  changeState ?  Icons.favorite_border : Icons.favorite,
+                  color: changeState ?  Colors.indigo : Colors.red,
+                ),
+              ),
+              top: (MediaQuery.of(context).size.height * 1 /4) - 32,
+              right: MediaQuery.of(context).size.width * 1 / 7,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:my_first_project/add_card.dart';
 
-void main() => runApp(HomePage());
+void main() => runApp(const HomePage());
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FirstScreen(),
     );
@@ -30,20 +31,37 @@ class _FirstScreenState extends State<FirstScreen> {
       body: Column(
         children: [
           Image.asset('assets/images/image1.png'),
-          Text(
+          const Text(
             "Title",
             style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
           ),
           Expanded(
             child: Container(
               margin: EdgeInsets.all(15),
-              child: SingleChildScrollView(
+              child: const SingleChildScrollView(
                 child: Text(
                   "The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders. The Flutter logo, in widget form. This widget respects the IconTheme. For guidelines on using the Flutter logo, visit https://flutter.dev/brand. See also: IconTheme, which provides ambient configuration for icons. Icon, for showing icons the Material design icon library. ImageIcon, for showing icons from AssetImages or other ImageProviders.",
                   style: TextStyle(fontSize: 20, color: Colors.grey),
                 ),
               ),
             ),
+          ),
+          AnimatedButton.strip(
+            width: 200,
+            height: 70,
+            text: 'افزودن کارت',
+            isReverse: true,
+            animatedOn: AnimatedOn.onHover,
+            backgroundColor: Colors.blue,
+            selectedTextColor: Colors.black,
+            stripTransitionType: StripTransitionType.RIGHT_TO_LEFT,
+            selectedBackgroundColor: Colors.white,
+            onPress: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddCard()),
+              );
+            },
           ),
           ElevatedButton(
               onPressed: () {
